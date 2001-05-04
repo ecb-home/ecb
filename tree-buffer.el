83218c6256230633cba779209fc8b860c0677a7b
berndl <berndl>
2001-05-04 13:21:44 +0000

    minor changes

--- tree-buffer.el
+++ tree-buffer.el
@@ -26,7 +26,7 @@
 ;; This file is part of the ECB package which can be found at:
 ;; http://home.swipnet.se/mayhem/ecb.html
 
-;; $Id: tree-buffer.el,v 1.35 2001/05/03 20:17:41 creator Exp $
+;; $Id: tree-buffer.el,v 1.36 2001/05/04 13:21:44 berndl Exp $
 
 ;;; Code:
 
@@ -231,9 +231,8 @@ NODE must be valid and already be visible in WINDOW!"
 
 (defun tree-buffer-highlight-node-data(node-data &optional dont-make-visible)
   (if node-data
-    (let* ((node (tree-buffer-find-node-data node-data))
-	   (w (get-buffer-window (current-buffer)))
-	   (ws (window-start w)))
+    (let ((node (tree-buffer-find-node-data node-data))
+          (w (get-buffer-window (current-buffer))))
       (when node
 ;;        (tree-buffer-remove-highlight)
         (setq tree-buffer-highlighted-node-data node-data)
