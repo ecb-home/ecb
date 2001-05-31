128783b6d50733c364f5a307263c863d3f6c560b
creator <creator>
2001-05-31 21:40:47 +0000

    updated to 1.31

--- html/ecb-html.el
+++ html/ecb-html.el
@@ -324,6 +324,8 @@
     (h-bullet-link-list
      ecb-bullet
      (list 
+      (list (concat ecb-download-url "ecb-1.30.zip") "ecb-1.30.zip")
+      (list (concat ecb-download-url "ecb-1.30.tar.gz") "ecb-1.30.tar.gz")
       (list (concat ecb-download-url "ecb-1.20.zip") "ecb-1.20.zip")
       (list (concat ecb-download-url "ecb-1.20.tar.gz") "ecb-1.20.tar.gz")
       (list (concat ecb-download-url "ecb-1.10.zip") "ecb-1.10.zip")
