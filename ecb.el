0ab52a7ae27e84a9f7d995635904d76e40447092
berndl <berndl>
2001-06-12 15:37:35 +0000

    more information in problem report

--- ecb.el
+++ ecb.el
@@ -7,7 +7,7 @@
 ;; Keywords: java, class, browser
 ;; Created: Jul 2000
 
-(defvar ecb-version "1.32"
+(defvar ecb-version "1.40prerelease"
   "Current ECB version.")
 
 ;; This program is free software; you can redistribute it and/or modify it under
