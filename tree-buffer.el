1d9e675c109032fb6e3fea880bdbeb4c36c4b70c
berndl <berndl>
2002-10-06 11:05:47 +0000

    added grep and grep-find command to popup menues

--- tree-buffer.el
+++ tree-buffer.el
@@ -666,14 +666,17 @@ mentioned above!"
   (when menu-items
     (cons
      (if running-xemacs
-	 (let ((v (make-vector 3 t)))
-	   (aset v 0 (caar menu-items))
-	   (aset v 1 (list (cadar menu-items)
-			   '(tree-buffer-get-node-at-point)))
-	   v)
+         (if (null (cdar menu-items))
+             (caar menu-items)
+           (let ((v (make-vector 3 t)))
+             (aset v 0 (caar menu-items))
+             (aset v 1 (list (cadar menu-items)
+                             '(tree-buffer-get-node-at-point)))
+             v))
        (cons (cadar menu-items)
 	     (cons (caar menu-items) t)))
      (tree-buffer-create-menu (cdr menu-items)))))
+
 
 (defun tree-buffer-create-menus (menus)
   "Creates a popup menus from an assoc list with menus."
