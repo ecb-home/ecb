570a8ff87ff7c83fbef74a020b158ff21199da0b
berndl <berndl>
2003-01-24 14:37:09 +0000

    new function ecb-read-number

--- ecb-util.el
+++ ecb-util.el
@@ -26,7 +26,7 @@
 ;; This file is part of the ECB package which can be found at:
 ;; http://home.swipnet.se/mayhem/ecb.html
 
-;; $Id: ecb-util.el,v 1.47 2003/01/22 10:12:51 berndl Exp $
+;; $Id: ecb-util.el,v 1.48 2003/01/24 14:37:09 berndl Exp $
 
 ;;; Code:
 
@@ -421,6 +421,24 @@ During the evaluation of BODY the following local variables are bound:
 
 (put 'ecb-do-if-buffer-visible-in-ecb-frame 'lisp-indent-function 1)
 
+(defun ecb-read-number (prompt &optional init-value)
+  "Ask in the minibuffer for a number with promptstring PROMPT. Optional
+INIT-VALUE can be either a number or a string-representation of a number."
+  (let ((init (cond ((numberp init-value)
+                     (number-to-string init-value))
+                    ((stringp init-value)
+                     (if (string= init-value "0")
+                         init-value
+                       (if (not (= 0 (string-to-number init-value)))
+                           init-value
+                         (ecb-error "ecb-read-number: init-value not a valid number!"))))
+                    (t nil)))
+        result)
+    (while (progn
+             (setq result (read-string prompt init))
+             (not (or (string= "0" result)
+                      (not (= 0 (string-to-number result)))))))
+    (string-to-number result)))
 
 (defun ecb-option-get-value (option &optional type)
   "Return the value of a customizable ECB-option OPTION with TYPE, where TYPE
