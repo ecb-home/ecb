ccb0b5c8a2f58ec886de4333f3f74ab9f25e21fe
berndl <berndl>
2003-03-31 15:40:18 +0000

    new option ecb-compilation-predicates

--- ecb-compilation.el
+++ ecb-compilation.el
@@ -1,6 +1,6 @@
 ;;; ecb-compilation.el --- 
 
-;; $Id: ecb-compilation.el,v 1.20 2003/03/20 16:43:32 berndl Exp $
+;; $Id: ecb-compilation.el,v 1.21 2003/03/31 15:40:22 berndl Exp $
 
 ;; Copyright (C) 2000-2003 Free Software Foundation, Inc.
 ;; Copyright (C) 2000-2003 Kevin A. Burton (burton@openprivacy.org)
@@ -104,13 +104,43 @@ window of ECB even if `compilation-buffer-p' says nil."
 
 (defvar ecb-compilation-major-modes-internal nil
   "This variable is for ECB internal use and can be used by ECB to add
-  major-mode symbols to the set displayed in the compile-window.")
+major-mode symbols to the set displayed in the compile-window.")
 
 (defun ecb-compilation-major-modes ()
   "Return all major-mode symbols which should be displayed in the
 compile-window."
   (append ecb-compilation-major-modes
           ecb-compilation-major-modes-internal))
+
+
+(defcustom ecb-compilation-predicates '(comint-check-proc)
+  "*Predicate when a buffer should be treaten as compilation-buffer.
+Every element of this list has to be a function or lambda-expression which
+gets as argument a buffer-object and which has to return not nil when this
+buffer should be treaten as compilation-buffer and therefore displayed in the
+compile-window of ECB \(if there is any).
+
+In combination with the values of `ecb-compilation-buffer-names' and
+`ecb-compilation-major-modes' ECB decides when a buffer is displayed in the
+compile-window.
+
+Default value is the function `comint-check-proc' which returns not nil when
+the buffer-argument is related to a living process."
+  :group 'ecb-compilation-content
+  :group 'ecb-layout
+  :type '(repeat (symbol :tag "Compilation predicate")))
+
+(defvar ecb-compilation-predicates-internal nil
+  "This variable is for ECB internal use and can be used by ECB to add
+predicates to the set defined in `ecb-compilation-predicates'.")
+
+(defun ecb-compilation-predicates ()
+  "Return all predicates which should be used to test if a buffer should be
+displayed in the compile-window."
+  (append ecb-compilation-predicates
+          ecb-compilation-predicates-internal))
+
+
 
 (defun ecb-compilation-get-buffers()
   "Get all known compilation buffer names.  See `ecb-compilation-buffer-p'."
@@ -135,7 +165,6 @@ compile-window."
 
     buffer-names))
 
-;; (ecb-compilation-registered-buffer-p "*Help: insert*")
 
 (defun ecb-compilation-buffer-p(buffer-or-name)
   "Test if the given buffer is a compilation buffer. Note that in this case we
@@ -157,8 +186,8 @@ is contained in the list returned by the function
                       ((bufferp buffer-or-name)
                        buffer-or-name)
                       (t
-                       nil))))
-
+                       nil)))
+        (ecb-comp-predicates (ecb-compilation-predicates)))
     (when buffer
 
       ;;test if this is a valid buffer by name.
@@ -172,11 +201,12 @@ is contained in the list returned by the function
           ;;else test if this is a regular compilation buffer
           (if (compilation-buffer-p buffer)
               t
-            ;;else check if this is a comint buffer
-            (if (and (fboundp 'comint-check-proc)
-                     (comint-check-proc buffer))
+            ;; we do not use run-hook-with-args-until-success because we have
+            ;; to check if the functions are bound!!
+            (if (dolist (p ecb-comp-predicates)
+                  (if (and (fboundp p) (funcall p buffer))
+                      (return t)))
                 t
-              ;;else it isn't a complication buffer
               nil)))))))
 
 ;; Klaus Berndl <klaus.berndl@sdm.de>: The following mechanism is necessary to
