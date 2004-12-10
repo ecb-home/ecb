21472610309e65e3775a6936e5aeac0ec75ef804
berndl <berndl>
2004-12-10 12:56:32 +0000

    new version number 2.31

--- ecb-upgrade.el
+++ ecb-upgrade.el
@@ -159,7 +159,7 @@
 
 ;; IMPORTANT: The version-number is auto-frobbed from the Makefile. Do not
 ;; change it here!
-(defconst ecb-version "2.31beta1"
+(defconst ecb-version "2.31"
   "Current ECB version.")
 
 (eval-when-compile
