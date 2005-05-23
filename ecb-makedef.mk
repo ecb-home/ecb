7482e988300e2027679d34077b4aacc2bae8f1b3
berndl <berndl>
2005-05-23 15:48:50 +0000

    new file ecb-symboldef.el added

--- ecb-makedef.mk
+++ ecb-makedef.mk
@@ -37,7 +37,8 @@ ecb_LISP_EL=tree-buffer.el ecb-util.el ecb-mode-line.el ecb-help.el \
             ecb-speedbar.el ecb-examples.el ecb-tod.el ecb-autogen.el \
 	    ecb-jde.el ecb-file-browser.el ecb-method-browser.el \
 	    ecb-winman-support.el ecb-semantic-wrapper.el \
-	    ecb-compatibility.el ecb-common-browser.el ecb-analyse.el
+	    ecb-compatibility.el ecb-common-browser.el ecb-analyse.el \
+	    ecb-symboldef.el
 
 ecb_LISP_ELC=$(ecb_LISP_EL:.el=.elc)
 
