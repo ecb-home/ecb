40c232acb1781d834d70540a3f4c87131c07bdb8
Ray Racine <ray.racine@gmail.com>
2012-09-05 12:34:14 -0400

    Update to use emacs internal semantic api vs cedet's.

--- ecb-semantic-wrapper.el
+++ ecb-semantic-wrapper.el
@@ -38,8 +38,6 @@
 
 
 (require 'semantic)
-(require 'semantic-ctxt)
-(require 'semantic-analyze)
 
 (defconst ecb-semantic-2-loaded (string-match "^2" semantic-version))
 (defconst ecb-semantic-2-beta-nr (if (and ecb-semantic-2-loaded
@@ -60,8 +58,6 @@
 (silentcomp-defvar semanticdb-search-system-databases)
 (silentcomp-defvar semantic-format-use-images-flag)
 (silentcomp-defvar ezimage-use-images)
-;; semantic 2.0 does not have this
-(silentcomp-defvar semantic-toplevel-bovine-cache)
 
 ;; -- getter functions for all variables of semantic currently used by ECB ---
 
@@ -285,7 +281,7 @@ unloaded buffer representation."
 (if (fboundp 'semantic-fetch-available-tags)
     (defalias 'ecb--semantic-fetch-available-tags 'semantic-fetch-available-tags)
   (defsubst ecb--semantic-fetch-available-tags ()
-    semantic-toplevel-bovine-cache))
+    semantic--buffer-cache))
 
 (if (fboundp 'semantic-tag-components)
     (defalias 'ecb--semantic-tag-components
